e7567816edf5fdf51f82feaef61f95e5d72a8818
TIAGO LAURENTINO DA CONCEICAO <108843416+tiagolaurentino@users.noreply.github.com>
2022-11-11 19:40:55 -0300

    Exercício de condições

---
+++ condiçoes/exer001.js
@@ -0,0 +1,8 @@
+
+
+var vel = 80
+console.log(`A velocidade do seu carro é ${vel} km por hora`)
+if(vel > 60) { // condição simples por não senão (else)
+    console.log (`Você ultrapassou a velocidade permitida. MULTADO !! `)
+}
+console.log(`Dirije sempre usando o cinto de segurança!!`)
---
+++ condiçoes/exer002.js
@@ -0,0 +1,8 @@
+
+var país = 'Brasil'
+console.log(`Vivendo em ${país}`)
+if(país == 'brasil'){
+    console.log(`Você é Brasileiro!`)
+}else{
+    console.log(`Você é estrangeiro!`)
+}
