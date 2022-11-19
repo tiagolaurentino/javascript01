f864c34e7af7b39919bab069ef77f06335b7711a
TIAGO LAURENTINO DA CONCEICAO <108843416+tiagolaurentino@users.noreply.github.com>
2022-11-19 17:37:25 -0300

    aula de repetição

---
+++ repetição/ambiente-for.js
@@ -0,0 +1,10 @@
+/*var c = 1
+while(c <= 6){
+    console.log(c)
+    c++
+} */
+       console.log('vai começar!!')
+for(var c = 1; c <= 6; c++ ) {
+    console.log(c)
+}
+console.log('FIM!!')
---
+++ repetição/ambienteteste.js
@@ -0,0 +1,21 @@
+/*console.log('tudo bem!!')
+console.log('tudo bem!!')
+console.log('tudo bem!!')
+console.log('tudo bem!!')
+console.log('tudo bem!!')
+console.log('tudo bem!!')
+console.log('tudo bem!!')*/
+
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1 
+} while (c <= 50)  // Faça isso tudo enquanto o contador for maior ou = a 6
+
+/*var c = 1
+while (c <= 6){
+    console.log('tudo bem!!')
+    c++ // c = c + 1
+}*/
+
+// tirando o = conta 5 vezes e com o = 6 vezes!!!
